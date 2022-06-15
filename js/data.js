47c5517f55693ec662878f4c9bf7eae5be1108c8
Collin Hovey <collinhovey@gmail.com>
2022-06-15 23:05:48 +0000

    Completed Issue 2

--- js/data.js
+++ js/data.js
@@ -2,18 +2,30 @@
 /* exported favorites */
 var data = {
   nextEntryId: 0,
-  library: [
-    {
-      pokemon: 'pikachu',
-      nickname: 'bolt',
-      encounters: 564,
-      picture: 'images/pikachu-f.png'
-    },
-    {
-      pokemon: 'Charizard',
-      nickname: 'Charmlizardo',
-      encounters: 2323,
-      picture: 'images/charizard.png'
-    }
-  ]
+  library: []
 };
+
+var favorites = [];
+
+window.addEventListener('beforeunload', saveData);
+pullData();
+
+function saveData(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('dataEntries', dataJSON);
+  var favoritesJSON = JSON.stringify(favorites);
+  localStorage.setItem('favoritesEntries', favoritesJSON);
+}
+
+function pullData(event) {
+  var savedDataJSON = localStorage.getItem('dataEntries');
+  var savedData = JSON.parse(savedDataJSON);
+  if (savedData !== null) {
+    data = savedData;
+  }
+  var savedFavoritesJSON = localStorage.getItem('favoritesEntries');
+  var savedFavorites = JSON.parse(savedFavoritesJSON);
+  if (savedFavorites !== null) {
+    favorites = savedFavorites;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
-var favorites = [
-  {
-    pokemon: 'pikachu',
-    nickname: 'bolt',
-    encounters: 564,
-    picture: 'images/pikachu-f.png'
-  },
-  {
-    pokemon: 'charizard',
-    nickname: 'charmlizardo',
-    encounters: 2323,
-    picture: 'images/charizard.png'
-  }
-];
-
+/* global data, favorites */
 var $favoritesView = document.querySelector('.carousel');
-// var $newCatchView = document.querySelector('.new-catch');
+var $newCatchView = document.querySelector('.new-catch');
 var $libraryView = document.querySelector('.library');
 var $favoritesButton = document.querySelector('.favorites-button');
-// var $newCatchButton = document.querySelector('.new-catch-button');
+var $newCatchButton = document.querySelector('.new-catch-button');
 var $libraryButton = document.querySelector('.library-button');
 var $carouselImage = document.querySelector('.favorite-image');
 var $carouselPokemon = document.querySelector('.carousel-pokemon');
@@ -27,25 +13,44 @@ var $carouselLeftButton = document.querySelector('div.left-button');
 var $carouselRightButton = document.querySelector('div.right-button');
 var $searchBar = document.querySelector('.search-bar');
 var $allEntries = document.querySelectorAll('.entry');
+var $newCatchForm = document.querySelector('.new-catch-form');
+var $cancelNewCatchButton = document.querySelector('button.new-catch-cancel');
+var $saveNewCatchButton = document.querySelector('button.new-catch-save');
+var $newCatchPokemon = document.querySelector('.new-catch-pokemon');
+var $newCatchNickName = document.querySelector('.new-catch-nickname');
+var $newCatchEncounters = document.querySelector('.new-catch-encounters');
 
 function switchFavorites(event) {
   clearInterval(stopCarousel);
   stopCarousel = setInterval(carouselSwitch, 5000);
   $libraryView.setAttribute('class', 'library hidden');
   $favoritesView.setAttribute('class', 'carousel');
+  $newCatchView.setAttribute('class', 'new-catch hidden');
 }
 
 function switchLibrary(event) {
   clearInterval(stopCarousel);
   $libraryView.setAttribute('class', 'library');
   $favoritesView.setAttribute('class', 'carousel hidden');
+  $newCatchView.setAttribute('class', 'new-catch hidden');
 }
+
+function switchNewCatch(event) {
+  clearInterval(stopCarousel);
+  $libraryView.setAttribute('class', 'library hidden');
+  $favoritesView.setAttribute('class', 'carousel hidden');
+  $newCatchView.setAttribute('class', 'new-catch');
+}
+
+$newCatchButton.addEventListener('click', switchNewCatch);
 
 $favoritesButton.addEventListener('click', switchFavorites);
 
 $libraryButton.addEventListener('click', switchLibrary);
 
-var stopCarousel = setInterval(carouselSwitch, 5000);
+if (favorites.length !== 0) {
+  var stopCarousel = setInterval(carouselSwitch, 5000);
+}
 
 var carouselCounter = 0;
 
@@ -108,3 +113,80 @@ function search(event) {
 }
 
 $searchBar.addEventListener('input', search);
+
+function cancelNewCatch() {
+  $libraryView.setAttribute('class', 'library');
+  $newCatchView.setAttribute('class', 'new-catch hidden');
+  $newCatchForm.reset();
+}
+
+function newLibraryEntry(entry) {
+  var $containerDiv = document.createElement('div');
+  $containerDiv.setAttribute('class', 'entry entry-' + entry.entryId);
+  var $image = document.createElement('img');
+  $image.setAttribute('class', 'entry-image');
+  $image.setAttribute('src', entry.picture);
+  $image.setAttribute('alt', entry.pokemon);
+  $containerDiv.appendChild($image);
+  var $entryNameDiv = document.createElement('div');
+  $entryNameDiv.setAttribute('class', 'entry-name');
+  $containerDiv.appendChild($entryNameDiv);
+  var $entryPokemon = document.createElement('h1');
+  $entryPokemon.setAttribute('class', 'entry-header');
+  $entryPokemon.textContent = entry.pokemon;
+  $entryNameDiv.appendChild($entryPokemon);
+  var $entryNickName = document.createElement('h1');
+  $entryNickName.setAttribute('class', 'entry-secondary');
+  $entryNickName.textContent = entry.nickname;
+  $entryNameDiv.appendChild($entryNickName);
+  var $entryEncounterDiv = document.createElement('div');
+  $entryEncounterDiv.setAttribute('class', 'entry-encounter');
+  $containerDiv.appendChild($entryEncounterDiv);
+  var $entryEncounter = document.createElement('h1');
+  $entryEncounter.setAttribute('class', 'entry-header');
+  $entryEncounter.textContent = 'Encounters';
+  $entryEncounterDiv.appendChild($entryEncounter);
+  var $entryEncounters = document.createElement('h1');
+  $entryEncounters.setAttribute('class', 'entry-secondary');
+  $entryEncounters.textContent = entry.encounters;
+  $entryEncounterDiv.appendChild($entryEncounters);
+  return $containerDiv;
+}
+
+function saveNewCatch(event) {
+  event.preventDefault();
+  var newShiny = {};
+  newShiny.pokemon = $newCatchPokemon.value;
+  newShiny.nickname = $newCatchNickName.value;
+  newShiny.encounters = parseInt($newCatchEncounters.value);
+  newShiny.entryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  function requestTest(name) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      newShiny.picture = xhr.response.sprites.front_shiny;
+      data.library.push(newShiny);
+      $libraryView.appendChild(newLibraryEntry(newShiny));
+      $libraryView.setAttribute('class', 'library');
+      $newCatchView.setAttribute('class', 'new-catch hidden');
+      $newCatchForm.reset();
+    });
+    xhr.send();
+  }
+  requestTest($newCatchPokemon.value.toLowerCase());
+}
+
+$cancelNewCatchButton.addEventListener('click', cancelNewCatch);
+$saveNewCatchButton.addEventListener('click', saveNewCatch);
+
+function loadLibrary() {
+  for (var x = 0; x < data.library.length; x++) {
+    if (data.library.length !== 0) {
+      $libraryView.appendChild(newLibraryEntry(data.library[x]));
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', loadLibrary);
